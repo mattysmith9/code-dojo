f3271cbbcfec95ff3ae94e7da80b34ca2c893f11
Matty Smith <matthewcsmith18@gmail.com>
2019-03-27 17:56:18 -0400

    prepare for deploy

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./Keys_prod');
+  module.exports = require('./keys_prod');
 } else {
-  module.exports = require('./Keys_dev');
+  module.exports = require('./keys_dev');
 }
--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const path = require('path');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
 
@@ -26,7 +26,9 @@ mongoose
 
 /* PASSPORT MIDDLEWAFRE */
 app.use(passport.initialize());
-require('./config/passport.js')(passport);
+
+/* PASSPORT CONFIG */
+require('./config/passport')(passport);
 
 /* USE ROUTES */
 app.use('/api/users', users);
@@ -47,4 +49,5 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
